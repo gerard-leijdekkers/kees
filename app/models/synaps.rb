5dbeab9a34ed18db47698469c47966c58cd57df4
Gerard Leijdekkers <gerard@eet.nu>
2015-06-01 09:31:05 +0200

    Added comments

--- app/models/synaps.rb
+++ app/models/synaps.rb
@@ -1,7 +1,11 @@
 class Synaps < ActiveRecord::Base
+  
+  ### ASSOCIATIONS:
   belongs_to :forward_neurons, foreign_key: :to
   belongs_to :backward_neurons, foreign_key: :from
   
+  
+  ### INSTANCE METHODS:
   def signal_forward
     forward_neurons.each do |neuron|
       neuron.fire
