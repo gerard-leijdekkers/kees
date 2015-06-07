6192c1445e07b2bfa0a4702eba668deda0e2f3a0
Gerard Leijdekkers <gerard@eet.nu>
2015-06-07 08:02:58 +0200

    Keep track of active neurons

--- app/models/brain.rb
+++ app/models/brain.rb
@@ -1,14 +1,29 @@
+
 class Brain
-  
+
+  MAX_SIMULTANIOUSLY_ACTIVE_NEURONS = 7
+
   # pattern input
   def self.process_pattern(pattern)
-    neurons = find_neurons(pattern)
+    neurons = fire_receptors(pattern)
+  end
+  
+  def self.add_active_neuron(neuron)
+    $active_neurons ||= []
+    
+    $active_neurons.push neuron
+    $active_neurons.shift if $active_neurons.size > MAX_SIMULTANIOUSLY_ACTIVE_NEURONS
+  end
+  
+  def self.active_neurons
+    $active_neurons ||= []
+    
+    $active_neurons.map { |n| n.to_char }
   end
   
   private
   
-  def self.find_neurons(pattern)
-    neurons = []
+  def self.fire_receptors(pattern)
     pattern.body.each_char do |char|
       CharacterReceptor.fire_for(char)
     end
--- app/models/neuron.rb
+++ app/models/neuron.rb
@@ -4,9 +4,13 @@ class Neuron < ActiveRecord::Base
   has_many :axons, class_name: 'Synaps', foreign_key: :from
   has_many :dendrites, class_name: 'Synaps', foreign_key: :to
   has_many :neurons, through: :axons
+  has_one  :character_receptor
   
   def fire
     logger.info "Neuron #{id} is firing".colorize(:green)
+
+    Brain.add_active_neuron self
+    
     axons.each do |synaps|
       synaps.signal_forward
     end
@@ -14,5 +18,11 @@ class Neuron < ActiveRecord::Base
     dendrites.each do |synaps|
       synaps.signal_backward
     end
+    
+    self
+  end
+  
+  def to_char
+    character_receptor.try(:character)
   end
 end
